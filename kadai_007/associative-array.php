bd1cad28ebbf717db0e71054417b9f43ccb20f42
nana0731 <138289930+nana0731@users.noreply.github.com>
2024-08-13 20:19:51 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+ 
+<head>
+     <meta charset="UTF-8">
+     <title>PHP基礎編</title>
+</head>
+ 
+<body>
+	<p>
+		<?php
+			$associative_array = [ 'name' => 'onion', 'price' => 200, 'weight' => 160];
+			print_r($associative_array);
+		?>
+	</p>
+</body>
+</html>
