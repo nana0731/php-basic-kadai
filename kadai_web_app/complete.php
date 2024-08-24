f91a45877e902fe4101f3e8566e6bc127cf14803
nana0731 <138289930+nana0731@users.noreply.github.com>
2024-08-24 16:09:40 +0900

    Add files via upload

---
+++ kadai_web_app/complete.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>PHP基礎編</title>
+</head>
+<body>
+	<h2>登録が完了しました。</h2>
+	<h3>社員情報がデータベースに保存されました。</h3>
+	<p>
+		<button id="back-btn" onclick="location.href='form.php';">戻る</button>
+	</p>
+</body>
+</html>
